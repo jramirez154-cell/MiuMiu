8833e5a9d1d7e084f04dbe06b3eca6734efe09d4
Alejandro Pinto <jramirez154@ucol.mx>
2026-05-26 19:07:22 -0600

    Creacion de Models(Tablas)

---
+++ Models/Ropa.cs
@@ -0,0 +1,32 @@
+﻿namespace MiuMiu.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; } = string.Empty;
+        public string telefono { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public DateTime fechaRegistro { get; set; } = DateTime.Now;
+    }
+
+    public class Venta
+    {
+        public int Id { get; set; }
+        public int clienteId { get; set; } 
+        public DateTime fecha { get; set; } = DateTime.Now;
+        public decimal total { get; set; }
+        public string estado { get; set; } = string.Empty;
+    }
+
+    public class Detalle
+    {
+        public int Id { get; set; }
+        public int ventaId { get; set; } 
+
+        // Aquí guardas el "Id" que viene de la clase ProductoTodos de la API
+        public int productoApiId { get; set; }
+
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+    }
+}
