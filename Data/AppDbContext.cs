ad80e3903b17c0e689c9bef5ebb41e17fe145825
Alejandro Pinto <jramirez154@ucol.mx>
2026-05-27 19:36:30 -0600

    Edicion de AppDbContext.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace MiuMiu.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MiuMiu.Models;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<DetalleVenta> DetallesVentas { get; set; }
+    }
+
+}
